14cf81ae4792e8b5cdafb0830fdebe89726b169b
Vedad Hadzic <vedad.hadzic@iaik.tugraz.at>
2022-08-25 13:22:41 +0200

    Made solver interchangeable

--- coco.cpp
+++ coco.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cassert>
+#include <iostream>
+
+extern "C" {
+#include "ipasir.h"
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    void* s = ipasir_init();
+    ipasir_add(s, 1);
+    ipasir_add(s, 0);
+
+    int ret = ipasir_solve(s);
+    assert(ret == 10);
+
+    int val = ipasir_val(s, 1);
+    std::cout << val << std::endl;
+
+    ipasir_release(s);
     return 0;
 }
